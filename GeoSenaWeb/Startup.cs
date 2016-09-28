20ceb5a50e3ed6ee93295c2a16bb895a06507fc4
Widres8 <geosenaparq@gmail.com>
2016-09-28 09:22:20 -0500

    Creacion Sitio

---
+++ GeoSenaWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GeoSenaWeb.Startup))]
+namespace GeoSenaWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
